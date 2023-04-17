e2c485eb50abaf8c7f944b174aea46693962ae0d
Lazaro Alonso <lazarus.alon@gmail.com>
2023-04-17 13:14:52 +0200

    anim globe data

--- src/globe_data_anim.jl
+++ src/globe_data_anim.jl
@@ -18,9 +18,16 @@ x,y,z = getSphere(lonext, lat, data_ext; h=0,k=0,m=0)
 d = Observable(data_ext .- 273.15)
 
 fig, ax, obj=surface(x, y, z;
-    color=d, colormap = Reverse(:Hiroshige),
+    color=d,
+    #colormap = Reverse(:Hiroshige),
+    colormap = [:snow2, :snow3, :white,  :grey50, :orange, :orangered, :orange, :yellow, :gold],
     colorrange=(-40,40),
     figure=(; resolution=(800,800), backgroundcolor=:grey10),
     axis=(; show_axis=false))
 zoom!(ax.scene, cameracontrols(ax.scene), 0.65)
 fig
+
+for t in axes(data,3)
+    d[] = ex_data(lon,lat,data[:,:,t]) .- 273.15
+    sleep(0.01)
+end
